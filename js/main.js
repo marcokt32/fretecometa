f7485f19009bd86042ddd276dd836f821d469920
Marco Quintela <quintelamarco94@gmail.com>
2026-08-19 15:11:50 -0300

    feat: estrutura inicial da landing page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 100) {
+        header.classList.add("visible");
+    } else {
+        header.classList.remove("visible");
+    }
+
+});
